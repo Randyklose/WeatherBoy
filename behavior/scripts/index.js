9ca765fba4ebe2d6537da45ef26c41214f71c988
Randy Klose <Randy@MacBooks-MacBook.local>
2017-02-17 13:50:36 -0500

    convo logic

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -34,16 +34,33 @@ exports.handle = (client) => {
   })
 
   client.runFlow({
-    classifications: {
-      // map inbound message classifications to names of streams
-    },
-    autoResponses: {
-      // configure responses to be automatically sent as predicted by the machine learning model
-    },
-    streams: {
-      main: 'onboarding',
-      onboarding: [sayHello],
-      end: [untrained],
-    },
-  })
+  classifications: {},
+  streams: {
+    main: 'getWeather',
+    hi: [sayHello],
+    getWeather: [collectCity, provideWeather],
+  }
+})
+  const collectCity = client.createStep({
+  satisfied() {
+    return Boolean(client.getConversationState().weatherCity)
+  },
+
+  prompt() {
+    // Need to prompt user for city    
+    console.log('Need to ask user for city')
+    client.done()
+  },
+})
+
+const provideWeather = client.createStep({
+  satisfied() {
+    return false
+  },
+
+  prompt() {
+    // Need to provide weather
+    client.done()
+  },
+})
 }
